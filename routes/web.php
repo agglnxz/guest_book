aee59da38b3cc64b94bc7bd3b8973061ecc4ec1b
lendiardlll <163377652+lendiardlll@users.noreply.github.com>
2024-03-16 09:36:59 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
 use App\Http\Controllers\LoginController;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
-
-=======
 use App\Http\Controllers\DashboardController;
 
 // Route::get('/', function () {
@@ -18,4 +9,4 @@ use App\Http\Controllers\DashboardController;
 // });
 
 Route::get('/',[DashboardController::class,'view_dashboard'])->name('dashboard');
->>>>>>> 6e1cbdc9cb95990d0b456a1d07528a91b043d227
+Route::post('/login', [LoginController::class, 'login'])->name('login');
